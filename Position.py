58abc459413572ecb9ac4016a4160fbb2707f41a
Ayse Idman <idmanayse@gmail.com>
2022-04-16 01:51:11 +0300

    Position class has been implemented.

--- Position.py
+++ Position.py
@@ -5,13 +5,20 @@ class Position:
     _pos = tuple[int, int]
 
     def __init__(self, row: int, col: int):
-        pass
+        if isinstance(row, int) and isinstance(col, int) and 0 <= row <= 23 and 0 <= col <= 59:
+            self._pos = row, col
+        else:
+            raise ValueError("__init__: invalid arguments")
 
     def __str__(self):
-        pass
+        return self._pos.__str__()
 
     def __getitem__(self, item: int) -> int:
-        pass
+        if isinstance(item, int) and (item == 0 or item == 1):
+            return self._pos[item]
+        raise ValueError("__getitem__: invalid arguments")
 
     def __eq__(self, other: Position):
-        pass
+        return self._pos == (other[0], other[1])
+
+
