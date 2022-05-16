872701ec5b50b116e5524576663453d928a61943
= <kaan.ari.tr@gmail.com>
2022-05-16 23:51:03 +0100

    CommandMessage Class has been implemented.

---
+++ wokwi/CommandMessage.py
@@ -0,0 +1,104 @@
+from Message import Message, MessageDestinationError
+
+
+class CommandMessage(Message):
+
+    def __init__(self, message):
+        super().__init__(message)
+        self._type = None
+        self._day = None
+        self._hour = None
+        self._minute = None
+        self._destination_node_id = None
+        self._source_node_id = None
+        self._message_id = None
+        self._cmd_parse()
+
+    def _cmd_parse(self):
+        self._validate()
+        self.type = self.body["cmd"]
+        self.day = self.body["day"]
+        self.destination_node_id = self.body["node_to"]
+        self.source_node_id = self.body["node_from"]
+        self.message_id = self.body["msg_id"]
+
+    @property
+    def type(self) -> str:
+        return self._type
+
+    @type.setter
+    def type(self, value: str):
+        hour_found = "hour" in self.body
+        minute_found = "minute" in self.body
+
+        if value == "GET-NODE-LOG-FULL":
+            pass
+        elif value == "GET-NODE-LOG-BY-HOUR" and hour_found:
+            pass
+        elif value == "GET-NODE-LOG-BY-MINUTE" and minute_found:
+            pass
+        elif value == "GET-ALL-LOG-FULL":
+            pass
+        else:
+            raise ValueError("Wrong Command Format!")
+
+        self._type = value
+
+    @property
+    def day(self) -> int:
+        return self._day
+
+    @day.setter
+    def day(self, value: str):
+        if not value.isnumeric():
+            raise ValueError("Wrong Day Format!")
+
+        int_value = int(value)
+        if int_value > 0:
+            raise ValueError("Wrong Day Format!")
+        self._day = int_value
+
+    @property
+    def hour(self) -> int:
+        return self._hour
+
+    @hour.setter
+    def hour(self, value: str):
+        if not value.isnumeric():
+            raise ValueError("Wrong Hour Format!")
+
+        int_value = int(value)
+        if int_value < 0 or int_value > 23:
+            raise ValueError("Wrong Hour Format!")
+
+        self._hour = int_value
+
+    @property
+    def minute(self) -> int:
+        return self._minute
+
+    @minute.setter
+    def minute(self, value: str):
+        if not value.isnumeric():
+            raise ValueError("Wrong Minute Format!")
+
+        int_value = int(value)
+        if int_value < 0 or int_value > 59:
+            raise ValueError("Wrong Minute Format!")
+
+        self._minute = int_value
+
+    @property
+    def destination_node_id(self) -> str:
+        return self._destination_node_id
+
+    @destination_node_id.setter
+    def destination_node_id(self, value: str):
+        if value not in ["ANY", Message.CLIENT_ID]:
+            raise MessageDestinationError()
+        self._destination_node_id = value
+
+    def _validate(self):
+        keys = ["day", "node_from", "node_to", "msg_id"]
+        if not all(key in self.body for key in keys):
+            raise ValueError("Wrong Command Format!")
