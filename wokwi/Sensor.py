afafd5a32b371ae4b519e5afa084e84a83c804e9
= <kaan.ari.tr@gmail.com>
2022-05-19 20:36:17 +0100

    Sensor class has been implemented.

---
+++ wokwi/Sensor.py
@@ -0,0 +1,23 @@
+from machine import Pin
+
+DEFAULT_PIN = 4
+
+
+class Sensor:
+
+    def __init__(self, pin=DEFAULT_PIN, rising_edge=True, falling_edge=False,callback=None):
+        self._pin = Pin(pin, Pin.IN)
+        self._rising_edge = rising_edge
+        self._falling_edge = falling_edge
+        self._callback = callback
+        self._interrupt_config()
+
+    def _interrupt_config(self):
+        if not (self._rising_edge or self._falling_edge):
+            raise ValueError("At least one edge is needed to be presented.")
+
+        if not callable(self._callback):
+            raise ValueError("Callback function must be callable")
+        trigger = Pin.IRQ_RISING if self._rising_edge else 0
+        trigger |= Pin.IRQ_FALLING if self._falling_edge else 0
+        self._pin.irq(trigger=trigger, handler=self._callback)
