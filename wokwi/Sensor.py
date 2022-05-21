2519a063e6e60b72b1f9867f2152b8a75affff87
Ayse Idman <idmanayse@gmail.com>
2022-05-21 03:24:51 +0300

    Workaround for Sensor callback

--- wokwi/Sensor.py
+++ wokwi/Sensor.py
@@ -1,6 +1,6 @@
 from machine import Pin
 
-DEFAULT_PIN = 4
+DEFAULT_PIN = 12
 
 
 class Sensor:
@@ -9,15 +9,18 @@ class Sensor:
         self._pin = Pin(pin, Pin.IN)
         self._rising_edge = rising_edge
         self._falling_edge = falling_edge
-        self._callback = callback
+        self._callback_route = callback
         self._interrupt_config()
 
     def _interrupt_config(self):
         if not (self._rising_edge or self._falling_edge):
             raise ValueError("At least one edge is needed to be presented.")
 
-        if not callable(self._callback):
+        if not callable(self._callback_route):
             raise ValueError("Callback function must be callable")
         trigger = Pin.IRQ_RISING if self._rising_edge else 0
         trigger |= Pin.IRQ_FALLING if self._falling_edge else 0
         self._pin.irq(trigger=trigger, handler=self._callback)
+
+    def _callback(self, _):
+        self._callback_route()
